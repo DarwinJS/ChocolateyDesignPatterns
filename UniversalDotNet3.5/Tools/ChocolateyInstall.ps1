d686327dfa6d24d0eeb042c88b86ac0dcc861725
Darwin Sanoy <DSanoy@workwave.com>
2016-01-19 00:47:59 +0000

    Tested working changes and rename

--- UniversalDotNet3.5/Tools/ChocolateyInstall.ps1
+++ UniversalDotNet3.5/Tools/ChocolateyInstall.ps1
@@ -1,25 +1,68 @@
-if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
+If(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
 
-  If (Windows 2012 or later)
-  {
-    If (Get-WIndowsFeature blah -eq "Removed")
-    { # set the appropriate policy keys to allow "Removed" features to be pulled from Microsoft
-      Write-Output "Dotnet feature is marked removed, making some registry adjustments to ensure it will be pulled from Microsoft."
-      $RegKey = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Servicing"
-      $read = "$RegKey\LocalSourcePath"
-      $read = "$RegKey\RepairContentServerSource"
+  If ([bool](dism /online /get-featureinfo /featurename:NetFx3 | Select-String "Payload Removed"))
+  { # set the appropriate policy keys to allow "Removed" features to be pulled from Microsoft
+    Write-Output "Dotnet feature is marked removed, making some registry adjustments to ensure it will be successfully pulled from Microsoft."
+    $RegKey = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Servicing"
 
-      Set LocalsourePath REGSZ_MULTI - blank
-      Set RepairContentServerSource Dword 2
+    If (!(Test-Path $RegKey))
+    {
+      New-Item $RegKey -Force | out-null
+      $SaveLocalKey = 'DidNotExist'
     }
+    else
+    {
+      If ([bool]((get-itemproperty $RegKey -ErrorAction SilentlyContinue).LocalSourcePath))
+      {
+        $SaveLocalSourcePath = (get-itemproperty $RegKey -ErrorAction SilentlyContinue).LocalSourcePath
+      }
+      else
+      {
+        $SaveLocalSourcePath = "DidNotExist"
+      }
+
+      If ([bool]((get-itemproperty $RegKey -ErrorAction SilentlyContinue).RepairContentServerSource))
+      {
+        $SaveRepairContentServerSource = (get-itemproperty $RegKey -ErrorAction SilentlyContinue).RepairContentServerSource
+      }
+      else
+      {
+        $SaveRepairContentServerSource = "DidNotExist"
+      }
+    }
+    New-ItemProperty -Path $RegKey -Name 'LocalSourcePath' -Type Multistring -Value '' -Force | out-null
+    New-ItemProperty -Path $RegKey -Name 'RepairContentServerSource' -Type DWord -Value 2 -Force | out-null
   }
+
 
   $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3 /All"
   $statements = "cmd.exe $packageArgs"
   Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
 
   #Set registry keys back to defaults
-
+  If ($SaveLocalKey -eq 'DidNotExist')
+  {
+    Remove-Item $RegKey -Recurse -Force
+  }
+  else
+  {
+    If ($SaveLocalSourcePath -eq 'DidNotExist')
+    {
+      Remove-ItemProperty -Path $RegKey -Name 'LocalSourcePath' -Force
+    }
+    else
+    {
+      New-ItemProperty -Path $RegKey -Name 'LocalSourcePath' -Type Multistring -Value $SaveLocalSourcePath -Force
+    }
+    If ($SaveRepairContentServerSource -eq 'DidNotExist')
+    {
+      Remove-ItemProperty -Path $RegKey -Name 'RepairContentServerSource' -Force
+    }
+    else
+    {
+      New-ItemProperty -Path $RegKey -Name 'RepairContentServerSource' -Type Multistring -Value $SaveLocalSourcePath -Force
+    }
+  }
 }
 else {
      Write-Host "Microsoft .Net 3.5 Framework is already installed on your machine."
