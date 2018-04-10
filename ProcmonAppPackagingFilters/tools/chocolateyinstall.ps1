af7896893de95bdd5c3c430a8cf7a1b6aa8f0983
Darwin <darwinjsgithub.services@sanoys.com>
2018-04-10 14:22:25 -0400

    adding procmonapppackagingfilters

---
+++ ProcmonAppPackagingFilters/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Continue';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+reg import "$toolsDir\Install-ProcmonAppPackagingFilters.reg"
---
+++ ProcmonAppPackagingFilters/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Continue';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+reg import "$toolsDir\Uninstall-ProcmonAppPackagingFilters.reg"
